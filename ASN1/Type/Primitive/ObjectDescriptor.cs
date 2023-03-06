85b47a8ddc44500a1e3c2d7860eac739cffa594e
MACTEPwar <shebanic95@gmail.com>
2023-03-06 11:49:26 +0200

    added: ObjectDescriptor

---
+++ ASN1/Type/Primitive/ObjectDescriptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASN1.Type.Primitive
+{
+    public class ObjectDescriptor : PrimitiveString
+    {
+        public ObjectDescriptor(string descriptor): base(descriptor)
+        {
+            Str = descriptor;
+            typeTag= TYPE_OBJECT_DESCRIPTOR;
+        }
+
+        public string Descriptor() => Str;
+    }
+}
